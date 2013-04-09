c61fed1ccec50e8215be96386c7a2de8f1934787
hbc <bcxxxxxx@gmail.com>
2013-04-09 17:21:39 +0800

    added editor theme

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,7 +1,13 @@
 require([], function() {
-    var editor = ace.edit('editor');
-    editor.setTheme('ace/theme/twilight');
-    editor.getSession().setMode('ace/mode/javascript');
+    var editor, desc, mb;
+    
+    editor = ace.edit('editor');
+    editor.setTheme('ace/theme/tomorrow_night');
+    editor.getSession().setMode('ace/mode/c_cpp');
+
+    desc = document.getElementById('description');
+    mb = document.getElementById('main-board');
+    desc.style.height = mb.style.height = window.innerHeight + 10 + 'px';
 
     console.log('hello world');
 });
