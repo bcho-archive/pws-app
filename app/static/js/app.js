537078cac7b0c5185a3ace410fc0e9d818ec6399
hbc <bcxxxxxx@gmail.com>
2013-04-10 14:54:33 +0800

    added highlight.js support

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,13 +1,28 @@
-require([], function() {
-    var editor, desc, mb;
-    
-    editor = ace.edit('editor');
-    editor.setTheme('ace/theme/tomorrow_night');
-    editor.getSession().setMode('ace/mode/c_cpp');
+require.config({
+    paths: {
+        'hjs': 'lib/highlight'
+    }
+});
 
-    desc = document.getElementById('description');
-    mb = document.getElementById('main-board');
-    desc.style.height = mb.style.height = window.innerHeight + 10 + 'px';
+require(['hjs'], function(hjs) {
+    var editor, desc, mb;
+ 
+    if (document.getElementById('editor')) {   
+        editor = ace.edit('editor');
+        editor.setTheme('ace/theme/tomorrow_night');
+        editor.getSession().setMode('ace/mode/c_cpp');
+
+        desc = document.getElementById('description');
+        mb = document.getElementById('main-board');
+        desc.style.height = mb.style.height = window.innerHeight + 10 + 'px';
+    } else {
+        var codes = document.getElementsByTagName('code'),
+            i;
+        for (i = 0;i < codes.length;i++) {
+            codes[i].innerHTML = hjs.highlight(
+                'cpp', codes[i].textContent).value;
+        }
+    }
 
     console.log('hello world');
 });
